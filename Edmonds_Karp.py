c3a38e0720e2078009c5012370923e37da02f75d
Neeraj Kulkarni <nee.rajkul@yahoo.co.in>
2017-11-08 12:47:08 -0800

    Modified Input and added conditions for incorrect vertices

--- Edmonds_Karp.py
+++ Edmonds_Karp.py
@@ -1,7 +1,26 @@
 g = {}
-for _ in xrange(input("Enter no. of edges")):
-    start, end, weight = raw_input("Enter input edge in the form (start, end and weight):").strip().split()
+l = raw_input("Enter vertices V (space separated)").strip().split()
+for i in l:
+    g[i] = {}
+for _ in xrange(input("Enter no. of edges E (start, end and weight):")):
+    start, end, weight = raw_input().strip().split()
     if start not in g:
-        g[start] = {}
+        print "Unknown Vertex" + start + ": Edge" + start + end + "Not added"
     g[start][end] = weight
 print g
+
+"""
+Input
+a b c d e
+
+8
+
+a b 1
+a c 4
+b c 3
+b d 2
+d b 1
+d c 5
+b e 2
+e d -2
+"""
